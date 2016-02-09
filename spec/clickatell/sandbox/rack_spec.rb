20b0e0f5f35454bf3865042a9601eadbd2117800
Grant Petersen-Speelman <grantspeelman@gmail.com>
2016-02-09 15:53:10 +0200

    update README and gemspec

--- spec/clickatell/sandbox/rack_spec.rb
+++ spec/clickatell/sandbox/rack_spec.rb
@@ -4,8 +4,4 @@ describe Clickatell::Sandbox::Rack do
   it 'has a version number' do
     expect(Clickatell::Sandbox::Rack::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
